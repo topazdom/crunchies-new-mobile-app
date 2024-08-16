d1708413f3d6e8e5a59f01f8c6bbcb893cc45916
samuel-akinboro <victorakinboro2003@gmail.com>
2024-08-16 19:28:15 +0100

    themed webview

--- app/webview.tsx
+++ app/webview.tsx
@@ -1,14 +1,17 @@
 import { WebView } from 'react-native-webview';
 import { SafeAreaView, StyleSheet } from 'react-native';
+import { ThemedView } from '@/components/ThemedView';
 
 export default function WebViewCon() {
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <WebView
-        style={styles.container}
-        source={{ uri: 'https://crunchies-nextjs-2.vercel.app/' }}
-      />
-    </SafeAreaView>
+    <ThemedView style={{flex: 1}}>
+      <SafeAreaView style={{flex: 1}}>
+        <WebView
+          style={styles.container}
+          source={{ uri: 'https://crunchies-nextjs-2.vercel.app/' }}
+        />
+      </SafeAreaView>
+    </ThemedView>
   );
 }
 
