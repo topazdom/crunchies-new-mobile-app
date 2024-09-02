2d1706d1265c26844ba3bb47aae30bac32282f8a
David Nzagha <zaghadon@gmail.com>
2024-09-01 23:33:55 -0700

    Improved Webview

--- app/webview.tsx
+++ app/webview.tsx
@@ -1,17 +1,47 @@
-import { SafeAreaView, StyleSheet } from 'react-native';
+import { ActivityIndicator, Linking, SafeAreaView, StyleSheet, View } from 'react-native';
+import React, { useRef } from 'react';
 
-import React from 'react';
+import HomeLoadingPage from '@/components/HomeLoadingPage';
+import { StatusBar } from 'expo-status-bar';
 import { ThemedView } from '@/components/ThemedView';
 import { WebView } from 'react-native-webview';
 
 export default function WebViewCon() {
+  const webViewRef = useRef<WebView>(null);
+
+  const runFirst = `
+      //alert("Injected");
+      window.isRNWebView=true
+      console.log("JSInjectWork") // note: this is required, or you'll sometimes get silent failures
+    `;
   return (
-    <ThemedView style={{flex: 1}}>
+    <ThemedView style={{flex: 1, marginTop: "7%"}}>
+      <StatusBar hidden={false} backgroundColor='red' translucent={true}  />
       <SafeAreaView style={{flex: 1}}>
         <WebView
+          pullToRefreshEnabled={true}
           style={styles.container}
-          source={{ uri: 'https://crunchies-nextjs-2.vercel.app/' }}
+          javaScriptEnabled
+          scalesPageToFit
+          injectedJavaScript={runFirst}
+          source={{ uri: 'https://b56a-2c0f-2a80-76-e610-59d0-4d7f-6bbb-4d1f.ngrok-free.app/' }}
+          //renderLoading={()=> <HomeLoadingPage />}
+          renderLoading={() => <View style={styles.overlay}>
+                                <ActivityIndicator size="large" color={"red"} />
+                            </View>}
+          onError={(event) => {
+            const url = event.nativeEvent.url;
+            if (
+              url.startsWith('intent://') ||
+              url.startsWith('tel:') ||
+              url.startsWith('mailto:')
+            ) {
+              webViewRef.current?.goBack();
+              Linking.openURL(url);
+            }
+          }}
         />
+        {/* <HomeLoadingPage /> */}
       </SafeAreaView>
     </ThemedView>
   );
@@ -20,5 +50,13 @@ export default function WebViewCon() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  overlay: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'rgba(255, 255, 255, 0.45)', // Transparent white overlay
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    zIndex: 50,
   },
 });
