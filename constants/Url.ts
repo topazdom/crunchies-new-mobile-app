afa20ec6d82657ca745c72a8a3caf953b767177b
Nzagha David Eze <33941830+zaghadon@users.noreply.github.com>
2024-10-21 08:41:33 -0700

    Switched To Live Base URL

--- constants/Url.ts
+++ constants/Url.ts
@@ -1,2 +1,2 @@
-//export const BASE_URL = "https://www.crunchiesonline.com/";
-export const BASE_URL = "https://crunchies-sandbox-delta.vercel.app/";
+export const BASE_URL = "https://www.crunchiesonline.com/";
+//export const BASE_URL = "https://crunchies-sandbox-delta.vercel.app/";
