4450e6dc8b650b7b5c748bbc307bede5c30721b0
Nzagha David Eze <33941830+zaghadon@users.noreply.github.com>
2024-10-30 08:42:27 -0700

    Configured iOS Build

--- app.config.js
+++ app.config.js
@@ -8,7 +8,12 @@ module.exports = ({ config }) => {
       },
       package: "com.crunchiesonline.customerapp",
         googleServicesFile: process.env.GOOGLE_SERVICES_JSON ?? config.android.googleServicesFile,
-      }
+      },
+      ios: {
+        bundleIdentifier: "com.crunchiesonline.crunchies.customers",
+        googleServicesFile: process.env.GOOGLE_SERVICES_PLIST ?? config.ios.googleServicesFile,
+        supportsTablet: true,
+      },
     };
   };
   
