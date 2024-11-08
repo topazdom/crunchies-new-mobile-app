2fafe7e0e070d861d5cc7de91ba9e2a53d6979df
Nzagha David Eze <33941830+zaghadon@users.noreply.github.com>
2024-11-08 06:02:22 -0800

    Changed Android VersionCode Config Parameter.

--- app.config.js
+++ app.config.js
@@ -1,19 +1,23 @@
+const { version } = require("react");
+
 module.exports = ({ config }) => {
-    return {
-      ...config,
-      android: {
-        adaptiveIcon: {
+  return {
+    ...config,
+    android: {
+      adaptiveIcon: {
         foregroundImage: "./assets/images/adaptive-icon.png",
         backgroundColor: "#ffffff",
       },
       package: "com.peachblossoms.crunchies.customers",
-        googleServicesFile: process.env.GOOGLE_SERVICES_JSON ?? config.android.googleServicesFile,
-      },
-      ios: {
-        bundleIdentifier: "com.crunchiesonline.crunchies.customers",
-        googleServicesFile: process.env.GOOGLE_SERVICES_PLIST ?? config.ios.googleServicesFile,
-        supportsTablet: true,
-      },
-    };
+      versionCode: 20,
+      googleServicesFile:
+        process.env.GOOGLE_SERVICES_JSON ?? config.android.googleServicesFile,
+    },
+    ios: {
+      bundleIdentifier: "com.crunchiesonline.crunchies.customers",
+      googleServicesFile:
+        process.env.GOOGLE_SERVICES_PLIST ?? config.ios.googleServicesFile,
+      supportsTablet: true,
+    },
   };
-  
+};
